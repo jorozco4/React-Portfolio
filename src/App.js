351399b351308d8ac7b8c7f293177ae0803d4056
Jesse Orozco <orozcojesse0@gmail.com>
2021-07-01 18:29:11 -0400

    created section for about me

--- src/App.js
+++ src/App.js
@@ -1,9 +1,10 @@
 import React from "react";
 import "./App.css";
+
 // import Navbar from "./Components/NavbarElements";
 // import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 // import Home from "./Components";
-// import About from "./pages/about";
+import About from "./pages/about";
 // import Portfolio from "./pages/portfolio";
 // import Contact from "./pages/contact";
 // import Resume from "./pages/resume";
@@ -11,32 +12,33 @@ import * as ReactBootStrap from "react-bootstrap";
 
 function App() {
   return (
-    <div className="App">
-      <ReactBootStrap.Navbar
-        collapseOnSelect
-        expand="lg"
-        bg="dark"
-        variant="dark"
-      >
-        <ReactBootStrap.Navbar.Brand href="#home">
-          Orozco-Portolio
-        </ReactBootStrap.Navbar.Brand>
-        <ReactBootStrap.Navbar.Toggle aria-controls="responsive-navbar-nav" />
-        <ReactBootStrap.Navbar.Collapse id="responsive-navbar-nav">
-          <ReactBootStrap.Nav className="mr-auto">
-            <ReactBootStrap.Nav.Link href="#about-me">
-              About Me
-            </ReactBootStrap.Nav.Link>
-            <ReactBootStrap.Nav.Link href="contact">
-              Contact
-            </ReactBootStrap.Nav.Link>
-            <ReactBootStrap.Nav.Link href="portfolio">
-              Portfolio
-            </ReactBootStrap.Nav.Link>
-            <ReactBootStrap.Nav.Link href="resume">
-              Resume
-            </ReactBootStrap.Nav.Link>
-            {/* <ReactBootStrap.NavDropdown
+    <>
+      <div className="App">
+        <ReactBootStrap.Navbar
+          collapseOnSelect
+          expand="lg"
+          bg="dark"
+          variant="dark"
+        >
+          <ReactBootStrap.Navbar.Brand href="#home">
+            Orozco-Portolio
+          </ReactBootStrap.Navbar.Brand>
+          <ReactBootStrap.Navbar.Toggle aria-controls="responsive-navbar-nav" />
+          <ReactBootStrap.Navbar.Collapse id="responsive-navbar-nav">
+            <ReactBootStrap.Nav className="mr-auto">
+              <ReactBootStrap.Nav.Link href="#about-me">
+                About Me
+              </ReactBootStrap.Nav.Link>
+              <ReactBootStrap.Nav.Link href="contact">
+                Contact
+              </ReactBootStrap.Nav.Link>
+              <ReactBootStrap.Nav.Link href="portfolio">
+                Portfolio
+              </ReactBootStrap.Nav.Link>
+              <ReactBootStrap.Nav.Link href="resume">
+                Resume
+              </ReactBootStrap.Nav.Link>
+              {/* <ReactBootStrap.NavDropdown
               title="Dropdown"
               id="collasible-nav-dropdown"
             >
@@ -54,8 +56,8 @@ function App() {
                 Separated link
               </ReactBootStrap.NavDropdown.Item>
             </ReactBootStrap.NavDropdown> */}
-          </ReactBootStrap.Nav>
-          {/* <ReactBootStrap.Nav>
+            </ReactBootStrap.Nav>
+            {/* <ReactBootStrap.Nav>
             <ReactBootStrap.Nav.Link href="#deets">
               More deets
             </ReactBootStrap.Nav.Link>
@@ -63,8 +65,22 @@ function App() {
               Dank memes
             </ReactBootStrap.Nav.Link>
           </ReactBootStrap.Nav> */}
-        </ReactBootStrap.Navbar.Collapse>
-      </ReactBootStrap.Navbar>
+          </ReactBootStrap.Navbar.Collapse>
+        </ReactBootStrap.Navbar>
+      </div>
+      <About />
+    </>
+  );
+}
+
+function Hello() {
+  const message = "Hello world! I am a React Component";
+  // The return statement contains something called "JSX"
+  // JSX is a syntax extension to Javascript that allows us to write HTML inside Javascript
+  // Expressions in JSX should be placed in curly braces {}
+  return (
+    <div className="container">
+      <h2>{message}</h2>
     </div>
   );
 }
